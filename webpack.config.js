8226ac94ec98983dfeed69f65d9f5740b5ebd4d7
vutran <vutran1412@icloud.com>
2019-04-30 15:19:23 -0700

    initial project config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './frontend/tbo.jsx',
+    output: {
+        path: path.resolve(__dirname, "app", "assets", "javascripts"),
+        filename: './bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
