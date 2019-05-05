6d27dbeec6c29d26546dfd467c6762932f3ef241
vutran <vutran1412@icloud.com>
2019-05-05 14:09:39 -0700

    create movie index container

--- frontend/components/movie/movie_index_container.js
+++ frontend/components/movie/movie_index_container.js
@@ -2,3 +2,17 @@ import React from 'react'
 import { connect } from 'react-redux'
 import MovieIndex from './movie_index'
 import { fetchMovies } from '../../actions/'
+
+const mapStateToProps = state => (
+    {
+        movies: Object.values(state.movies)
+    }
+)
+
+const mapDispatchToProps = dispatch => (
+    {
+        fetchMovies: () => dispatch(fetchMovies())
+    }
+)
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieIndex)
