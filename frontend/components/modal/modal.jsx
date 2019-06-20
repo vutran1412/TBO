17fa147369092bf73a39427fc4139d618b01ed18
Vu Tran <vutran1412@icloud.com>
2019-06-20 11:24:57 -0700

    login and signup are now inside modal component

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,36 +1,43 @@
 import { connect } from "http2";
-
 import React from 'react'
 import { modalClose } from '../../actions/modal_actions'
 import { connect } from 'react-redux'
+import LoginFormContainer from '../session/login_form_container'
+import SignUpFormContainer from '../session/signup_form_container'
 
 const Modal = ({ modal }) => {
     if (!modal) return null
     let component
     switch(modal) {
+        case 'login':
+            component = <LoginFormContainer />
+            break
+        case 'signup':
+            component = <SignUpFormContainer />
+            break
         default:
             return null
     }
+
+    return (
+        <div className="modal-background">
+            <div className="modal-child" onClick={e => e.stopPropagation()}>
+                {component}
+            </div>
+        </div>
+    )
 }
 
-return (
-    <div className="modal-background">
-        <div className="modal-child" onClick={e => e.stopPropagation()}>
-            {component}
-        </div>
-    </div>
-)
-
-const mapStateToProps = state => {
-    return {
+const mapStateToProps = state => (
+    {
         modal: state.ui.modal
     }
-}
+)
 
-const mapDispatchToProps = dispatch => {
-    return {
+const mapDispatchToProps = dispatch => (
+    {
         modalClose: () => dispatch(modalClose())
     }
-}
+)
 
 export default connect(mapStateToProps, mapDispatchToProps)(Modal)
